b0e91e714a22fce1fa687eabc3d04fb164807310
Jakub Jedlicka <jjedlick@redhat.com>
2025-07-16 22:44:01 +0200

    Test 9

--- quarkus-test-openshift/src/main/java/io/quarkus/test/bootstrap/inject/OpenShiftClient.java
+++ quarkus-test-openshift/src/main/java/io/quarkus/test/bootstrap/inject/OpenShiftClient.java
@@ -53,7 +53,6 @@ import io.fabric8.knative.client.KnativeClient;
 import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
 import io.fabric8.kubernetes.api.model.ConfigMapVolumeSourceBuilder;
 import io.fabric8.kubernetes.api.model.Container;
-import io.fabric8.kubernetes.api.model.ContainerPort;
 import io.fabric8.kubernetes.api.model.EnvVar;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.IntOrString;
@@ -66,7 +65,7 @@ import io.fabric8.kubernetes.api.model.VolumeBuilder;
 import io.fabric8.kubernetes.api.model.VolumeMount;
 import io.fabric8.kubernetes.api.model.VolumeMountBuilder;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
-import io.fabric8.kubernetes.api.model.apps.DeploymentStatus;
+import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.kubernetes.client.dsl.NamespaceListVisitFromServerGetDeleteRecreateWaitApplicable;
@@ -355,14 +354,32 @@ public final class OpenShiftClient {
      * Changes the deployment spec to make the specific port exposed.
      */
     public void exposeDeploymentPort(String deploymentName, String portName, int port) {
-        Deployment deployment = client.apps().deployments().withName(deploymentName).get();
-        DeploymentStatus dep = deployment.getStatus();
+        //Deployment deployment = client.apps().deployments().withName(deploymentName).get();
+        Deployment updatedDeployment = client.apps().deployments()
+                .withName(deploymentName)
+                .edit(d -> new DeploymentBuilder(d)
+                        .editSpec()
+                        .editTemplate()
+                        .editSpec()
+                        // Edit the container at the specified index
+                        .editContainer(0)
+                        // Add a new port using the builder
+                        .addNewPort()
+                        .withName(portName)
+                        .withContainerPort(port)
+                        .withProtocol("TCP")
+                        .endPort() // Finish editing the port
+                        .endContainer() // Finish editing the container
+                        .endSpec()
+                        .endTemplate()
+                        .endSpec()
+                        .build());
 
-        deployment.getSpec().getTemplate().getSpec().getContainers().get(0).getPorts().add(
-                new ContainerPort(port, "", 0, portName, "TCP"));
+        //deployment.getSpec().getTemplate().getSpec().getContainers().get(0).getPorts().add(
+        //        new ContainerPort(port, "", 0, portName, "TCP"));
 
-        Log.info("Exposing port %d with name %s on deployment %s", port, portName, deploymentName);
-        client.resource(deployment).serverSideApply();
+        //Log.info("Exposing port %d with name %s on deployment %s", port, portName, deploymentName);
+        //client.resource(deployment).serverSideApply();
     }
 
     /**
