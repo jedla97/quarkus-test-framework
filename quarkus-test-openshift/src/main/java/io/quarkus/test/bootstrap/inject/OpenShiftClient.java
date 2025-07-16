89023f33469015515095a7abe2c562229ae9b532
Jakub Jedlicka <jjedlick@redhat.com>
2025-07-16 23:54:17 +0200

    Test 12

--- quarkus-test-openshift/src/main/java/io/quarkus/test/bootstrap/inject/OpenShiftClient.java
+++ quarkus-test-openshift/src/main/java/io/quarkus/test/bootstrap/inject/OpenShiftClient.java
@@ -127,7 +127,7 @@ public final class OpenShiftClient {
     private final KnativeClient kn;
     private final String scenarioId;
     private boolean isClientReady;
-    private long lastObservedGeneration;
+    private long lastObservedGeneration = 0;
 
     private OpenShiftClient(String scenarioId) {
         this.scenarioId = scenarioId;
@@ -377,6 +377,8 @@ public final class OpenShiftClient {
         if (initializedDeployment == null) {
             fail("Fetch of deployment failed");
         }
+
+        lastObservedGeneration = initializedDeployment.getStatus().getObservedGeneration();
         return initializedDeployment;
     }
 
